07f8b673a21626ba7489af517f0891c319f4fc7c
Stefano <stefano0012@gmail.com>
2019-07-11 15:08:05 +0200

    create SecurityController and login template

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,20 @@ class UserFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user
+            ->setEmail("user@example.com")
+            ->setFirstName("user")
+        ;
+
+        $admin = new User();
+        $admin
+            ->setEmail("admin@example.com")
+            ->setFirstName("admin")
+        ;
+
+        $manager->persist($user);
+        $manager->persist($admin);
 
         $manager->flush();
     }
